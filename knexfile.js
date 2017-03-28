fbd4196525588b02b9785f2b4ddb987f5c3530ec
davidliang781205 <davidliang781205@gmail.com>
2017-03-28 22:43:02 +0000

    Added migartion

---
+++ knexfile.js
@@ -0,0 +1,57 @@
+const settings = require('./settings'); // settings.json
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user: settings.user,
+      password: settings.password,
+      database: settings.database,
+      host: settings.hostname,
+      port: settings.port,
+      ssl: settings.ssl
+    },
+    searchPath: 'public'
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      user: settings.user,
+      password: settings.password,
+      database: settings.database,
+      host: settings.hostname,
+      port: settings.port,
+      ssl: settings.ssl
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      user: settings.user,
+      password: settings.password,
+      database: settings.database,
+      host: settings.hostname,
+      port: settings.port,
+      ssl: settings.ssl
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
